c29f7a22fedc3fa9029754ce83bdfc32dd4e1df4
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2018-01-02 12:08:39 +0100

    feat: UdaciStatusBar; utils/colors
    Added UdaciStatusBar Component (setup main app view to use it)
    Added utils/colors

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+import { Constants } from 'expo'
+import { black } from './utils/colors';
+
+function UdaciStatusBar({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.app}>
+        <UdaciStatusBar backgroundColor={black} barStyle="light-content" />
+        <View style={styles.container}>
+          <Text> UdaciCards </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  app: {
+    flex: 1
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ utils/colors.js
@@ -0,0 +1,3 @@
+export const white = '#fff'
+export const gray = '#757575'
+export const black = '#000'

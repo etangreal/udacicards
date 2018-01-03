8671b8958f33a5c5ce216b4c4af7f8371fc76c87
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2018-01-03 16:17:05 +0100

    feat: DeckListItem => # of Cards

--- components/Deck.ListItem.js
+++ components/Deck.ListItem.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native'
+import { black, gray } from '../utils/colors'
 
 export default function DeckListItem({ deck }) {
   return (
     <View key={deck.title}>
-      <Text>{deck.title}</Text>
+      <Text style={styles.title}> {deck.title} </Text>
+      <Text style={styles.cards}> {deck.questions.length} cards </Text>
+      <Text></Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    color: black,
+    fontSize: 25
+  },
+  cards: {
+    color: gray,
+    fontSize: 16
+  }
+})

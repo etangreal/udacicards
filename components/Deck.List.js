4a898f1eec8e8c219e8a68c39259e7860d3d1410
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2018-01-03 15:44:48 +0100

    feat: DeckList connects to Redux Store; displays list of decks

--- components/Deck.List.js
+++ components/Deck.List.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native'
+import { connect } from 'react-redux'
 
-export default function DeckList() {
-  return (
-    <View>
-      <Text> Deck List View </Text>
-    </View>
-  )
+function DeckList({ decks }) {
+  const list = decks.map(deck => {
+    return (
+      <View key={deck.title}>
+        <Text>{deck.title}</Text>
+      </View>
+    )
+  })
+
+  return <View style={styles.container}>
+    {list}
+  </View>
 }
+
+function mapStateToProps(state, props) {
+  const decks = Object.values(state);
+
+  return {
+    decks
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default connect(mapStateToProps)(DeckList);

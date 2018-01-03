ce0b8ca86e5550d951bf63dd577f6731bbac204c
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2018-01-03 16:35:41 +0100

    style: DeckListItem

--- components/Deck.ListItem.js
+++ components/Deck.ListItem.js
@@ -8,21 +8,31 @@ import { black, gray } from '../utils/colors'
 
 export default function DeckListItem({ deck }) {
   return (
-    <View key={deck.title}>
-      <Text style={styles.title}> {deck.title} </Text>
-      <Text style={styles.cards}> {deck.questions.length} cards </Text>
-      <Text></Text>
+    <View key={deck.title} style={styles.listItem}>
+      <View style={styles.listDetail}>
+        <Text style={styles.title}> {deck.title} </Text>
+        <Text style={styles.cards}> {deck.questions.length} cards </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  listItem: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  listDetail: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   title: {
     color: black,
     fontSize: 25
   },
   cards: {
     color: gray,
-    fontSize: 16
+    fontSize: 20
   }
 })

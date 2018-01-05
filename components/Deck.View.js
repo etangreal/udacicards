b5ba10b125b041101ace3f4424f590ec0a4fd28f
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2018-01-05 03:32:19 +0100

    Fix: DeckView => retrieves deck from redux store

--- components/Deck.View.js
+++ components/Deck.View.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
 import {
   View,
   Text,
@@ -8,8 +9,7 @@ import {
 import Button from './Button'
 import { white, gray, black } from '../utils/colors'
 
-export default function DeckView({ navigation }) {
-  const deck = navigation.state.params.deck;
+const DeckView = ({ deck, navigation }) => {
   const count = deck.questions.length;
   const enabled = { color: !count ? gray : black }
 
@@ -35,6 +35,16 @@ DeckView.navigationOptions = ({ navigation }) => {
     title: navigation.state.params.deck.title
   }
 }
+
+function mapStateToProps(state, { navigation }) {
+  const title = navigation.state.params.deck.title;
+
+  return {
+    deck: state[title]
+  }
+}
+
+export default connect(mapStateToProps)(DeckView);
 
 const styles = StyleSheet.create({
   container: {
